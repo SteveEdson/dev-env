976c657bcd6310c4c56945ef86c6037ccbebfc61
Steve Edson <steve3d50n@gmail.com>
2015-02-06 23:30:35 +0000

    added example test, and test script

---
+++ test/ExampleTest.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @author: Steve - G Media 2015
+ * 
+ */
+
+namespace test;
+
+
+class ExampleTest extends \PHPUnit_Framework_TestCase {
+
+    public function testPHPUnitWorking() {
+        $this->assertTrue(true, true);
+    }
+}
+ 
