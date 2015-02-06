1eb4f76ea1faa23a6371cc0edcc5e9deec3ac4cd
Steve Edson <steve3d50n@gmail.com>
2015-02-06 23:56:29 +0000

    updated test case

--- test/ExampleTest.php
+++ test/ExampleTest.php
@@ -6,9 +6,15 @@
 
 namespace test;
 
+class helloWorld {
+
+}
 
 class ExampleTest extends \PHPUnit_Framework_TestCase {
 
+    /**
+     * @covers \test\helloWorld
+     */
     public function testPHPUnitWorking() {
         $this->assertTrue(true, true);
     }
